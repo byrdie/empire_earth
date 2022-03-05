af091a7b4d3771fd8d2bfa13a4613f66ba861e23
Roy Smart <roytsmart@gmail.com>
2022-03-05 11:41:40 -0700

    Added inital implementation of `empire_earth.units.Unit` class.

--- empire_earth/databases/__init__.py
+++ empire_earth/databases/__init__.py
@@ -1,20 +1,156 @@
 import pathlib
+import struct
 import pandas
 
 path_base = pathlib.Path(__file__).parent
 
 
-def dbobject():
-    return pandas.read_csv(path_base / 'object_export.csv')
+def dbobjects_struct() -> pandas.DataFrame:
+    return pandas.read_csv(
+        path_base / 'dbobject_struct.csv',
+        names=['field name', 'cX', 'num_bytes', 'dtype'],
+        index_col=0,
+    )
 
 
-def family():
-    return pandas.read_csv(path_base / 'family_export.csv')
+def dbobjects() -> pandas.DataFrame:
+    dbobjects_structure = dbobjects_struct()
+
+    columns = [index for index, row in dbobjects_structure.iterrows()]
+
+    result = []
+
+    with open(path_base / 'dbobjects.dat', 'rb') as file:
+
+        file.read(4)
+
+        escape = False
+        while True:
+
+            result_row = []
+
+            for index, row in dbobjects_structure.iterrows():
+                num_bytes = row['num_bytes']
+                datum = file.read(num_bytes)
+                if not datum:
+                    escape = True
+                    break
+                dtype = row['dtype']
+                if dtype == 1:
+                    if num_bytes == 4:
+                        value = struct.unpack('i', datum)[0]
+                    else:
+                        value = datum.decode(errors='ignore')
+                elif dtype == 2:
+                    value = datum.decode(errors='ignore')
+                    value = value.replace('\x00', '')
+                elif dtype == 3:
+                    value = struct.unpack('f', datum)[0]
+                result_row.append(value)
+
+            if escape:
+                break
+
+            result.append(result_row)
+
+    result = pandas.DataFrame(result, columns=columns)
+
+    return result.set_index('DB Name')
 
 
-def epoch_span():
-    return pandas.read_csv(path_base / 'epoch_span.csv')
+def dbfamily() -> pandas.DataFrame:
+
+    columns = ['family name', 'c1', 'family index']
+
+    result = []
+
+    num_fields = 71
+
+    with open(path_base / 'dbfamily.dat', 'rb') as file:
+
+        file.read(4)
+
+        escape = False
+        while True:
+            result_row = []
+
+            for i in range(num_fields):
+                if i == 0:
+                    num_bytes = 100
+                else:
+                    num_bytes = 4
+                datum = file.read(num_bytes)
+                if not datum:
+                    escape = True
+                    break
+                if i == 0:
+                    value = datum.decode('utf-8').replace('\x00', '')
+                elif i < 3:
+                    value = struct.unpack('i', datum)[0]
+                else:
+                    value = struct.unpack('i', datum)[0] / 100
+                result_row.append(value)
+
+            if escape:
+                break
+
+            result.append(result_row)
+
+    columns = columns + list(range(num_fields - len(columns)))
+
+    return pandas.DataFrame(result, columns=columns).set_index('family index')
 
 
-def attack_multiplier():
-    return pandas.read_csv(path_base / 'attack_mult_export.csv')
+def epoch_span() -> pandas.DataFrame:
+    return pandas.read_csv(path_base / 'epoch_span.csv', index_col=0)
+
+
+def dbweapontohit() -> pandas.DataFrame:
+    columns = ['Weapon Hit Name', 'c1', 'Weapon Hit ID']
+
+    result = []
+
+    num_fields = 9
+
+    with open(path_base / 'dbweapontohit.dat', 'rb') as file:
+
+        file.read(4)
+
+        escape = False
+        while True:
+            result_row = []
+
+            for i in range(num_fields):
+                if i == 0:
+                    num_bytes = 100
+                else:
+                    num_bytes = 4
+                datum = file.read(num_bytes)
+                if not datum:
+                    escape = True
+                    break
+                if i == 0:
+                    value = datum.decode('utf-8').replace('\x00', '')
+                    # columns.append(value)
+                elif i < 3:
+                    value = struct.unpack('i', datum)[0]
+                else:
+                    value = struct.unpack('i', datum)[0] / 100
+                result_row.append(value)
+
+            if escape:
+                break
+
+            result.append(result_row)
+
+            columns = columns + list(range(num_fields - len(columns)))
+
+    return pandas.DataFrame(result, columns=columns).set_index('Weapon Hit ID')
+
+
+def weapon_to_hit_map() -> pandas.DataFrame:
+    return pandas.read_csv(path_base / 'weapontohit_export.csv', index_col=2)['Unnamed: 0']
+
+
+def language() -> pandas.DataFrame:
+    return pandas.read_csv(path_base / 'Language.txt', names=['value'], index_col=0)
--- empire_earth/units.py
+++ empire_earth/units.py
@@ -1,3 +1,109 @@
+from typing import List, Dict
 import dataclasses
+from empire_earth import databases
+
+dbobjects = databases.dbobjects()
+dblanguage = databases.language()
+dbfamily = databases.dbfamily()
+dbweapontohit = databases.dbweapontohit()
 
 
+@dataclasses.dataclass
+class Attack:
+    strength: float = 0
+    seconds_per_hit: float = 0
+    mode: int = 0
+    weapon_hit_id: int = 0
+
+
+@dataclasses.dataclass
+class Armor:
+    shock: float = 0
+    arrow: float = 0
+    pierce: float = 0
+    gun: float = 0
+    laser: float = 0
+    missile: float = 0
+
+
+@dataclasses.dataclass
+class Unit:
+    name: str = ''
+    id: int = 0
+    family: int = 0
+    hitpoints: float = 0
+    attack: Attack = dataclasses.field(default_factory=Attack)
+    armor: Armor = dataclasses.field(default_factory=Armor)
+    epoch_start: int = 0
+    epoch_stop: int = 0
+
+    def attack_multiplier_family(self, defender: 'Unit'):
+        return dbfamily.loc[defender.family, self.attack.mode]
+
+    def attack_multiplier_weapontohit(self, defender: 'Unit'):
+        return dbweapontohit.loc[defender.attack.weapon_hit_id, self.attack.weapon_hit_id]
+
+    def attack_multiplier(self, defender: 'Unit'):
+        return self.attack_multiplier_family(defender) * self.attack_multiplier_weapontohit(defender)
+
+    def attack_final(self, defender: 'Unit'):
+        return self.attack.strength * self.attack_multiplier(defender)
+
+    def armor_final(self, attacker: 'Unit'):
+        weapon_hit_id = attacker.attack.weapon_hit_id
+        if weapon_hit_id == 0:
+            return self.armor.shock
+        elif weapon_hit_id == 1:
+            return self.armor.arrow
+        elif weapon_hit_id == 2:
+            return self.armor.pierce
+        elif weapon_hit_id == 3:
+            return self.armor.gun
+        elif weapon_hit_id == 4:
+            return self.armor.laser
+        elif weapon_hit_id == 5:
+            return self.armor.missile
+        else:
+            raise ValueError
+
+    def damage_dealt_per_hit(self, defender: 'Unit'):
+        return self.attack_final(self) - defender.armor_final(self)
+
+    def damage_dealt_per_second(self, defender: 'Unit'):
+        return self.damage_dealt_per_hit(defender) / self.attack.seconds_per_hit
+
+    def fractional_damage_dealt_per_second(self, defender: 'Unit'):
+        return self.damage_dealt_per_second(defender) / defender.hitpoints
+
+
+def units_all() -> Dict[str, Unit]:
+
+    result = dict()
+
+    for index, row in dbobjects.iterrows():
+
+        id_language = row['Language ID']
+        if id_language in dblanguage.index:
+            name = dblanguage.loc[id_language].to_numpy()[0].replace('"', '').lstrip(' ')
+            unit = Unit(
+                name=name,
+                id=row['Object ID'],
+                family=row['Family'],
+                hitpoints=row['Health'],
+                attack=Attack(
+                    strength=row['Attack'],
+                    seconds_per_hit=row['Attack Speed'],
+                    mode=row['Attack Mode'],
+                    weapon_hit_id=row['Weapon Hit ID'],
+                ),
+                armor=Armor(
+                    shock=row['Shock Armor'],
+                    pierce=row['Pierce Armor'],
+                    gun=row['Gun Armor'],
+                    laser=row['Laser Armor'],
+                    missile=row['Missile Armor'],
+                ),
+            )
+            result[name] = unit
+
+    return result
