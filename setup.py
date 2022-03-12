7b226878972446ef7403548797e5f096edb465bd
Roy Smart <roytsmart@gmail.com>
2022-03-12 12:44:01 -0600

    Update setup.py

--- setup.py
+++ setup.py
@@ -12,13 +12,13 @@ setup(
         'astropy-sphinx-theme',
         'jupyter-sphinx',
     ],
-    package_data=[
-        ('empire_earth/databases', [
+    package_data={
+        'empire_earth/databases': [
             'empire_earth/databases/*.dat',
             'empire_earth/databases/*.csv',
             'empire_earth/databases/*.txt',
-        ]),
-    ],
+        ],
+    },
     include_package_data=True,
     url='https://github.com/byrdie/empire_earth',
     license='',
