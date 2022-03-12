634b56b6add0d5af734017a4fbd1260a68b1e582
Roy Smart <roytsmart@gmail.com>
2022-03-11 22:34:50 -0700

    Added skeleton for Sphinx documentation.

---
+++ docs/index.rst
@@ -0,0 +1,20 @@
+Introduction
+============
+
+This library aims to compute unit relationships for the game Empire Earth.
+
+.. autosummary::
+    :toctree: _autosummary
+    :template: module_custom.rst
+    :recursive:
+
+    empire_earth
+
+
+
+Indices and tables
+==================
+
+* :ref:`genindex`
+* :ref:`modindex`
+* :ref:`search`
---
+++ setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='empire_earth',
+    version='0.0.1',
+    packages=find_packages(),
+    install_requires=[
+        'pytest',
+        'numpy',
+        'pandas',
+        'sphinx-autodoc-typehints',
+        'astropy-sphinx-theme',
+        'jupyter-sphinx',
+    ],
+    url='https://github.com/byrdie/empire_earth',
+    license='',
+    author='Roy Smart',
+    author_email='roytsmart@gmail.com',
+    description='Utilities for calculating unit relationships in the game Empire Earth.'
+)
