7a6b2793c8b8749ede01b6ed5877ac227a551840
Roy Smart <roytsmart@gmail.com>
2022-03-14 18:06:37 -0600

    Added function to calculate docstrings dynamically

---
+++ empire_earth/units/relationships/epochs/_doc.py
@@ -0,0 +1,44 @@
+def _calc(epoch: int):
+    return f"""
+=====
+Units
+=====
+
+.. jupyter-execute::
+
+    import pandas
+    pandas.set_option('display.max_columns', 1000)
+    pandas.set_option('display.max_rows', 1000)
+    pandas.set_option('display.max_colwidth', 200)
+
+    import empire_earth.units.relationships.matrices
+
+    epoch = {epoch}
+    units = empire_earth.units.attackers(epoch)
+    pandas.DataFrame(units.values())
+
+===========================
+Damage Dealt per Hit
+===========================
+
+.. jupyter-execute::
+
+    empire_earth.units.relationships.matrices.matrix_damage_dealt_per_hit(units.values())
+
+===========================
+Tactical Unit Relationships
+===========================
+
+.. jupyter-execute::
+
+    empire_earth.units.relationships.matrices.matrix_tactical(units.values())
+
+============================
+Strategic Unit Relationships
+============================
+
+.. jupyter-execute::
+
+    empire_earth.units.relationships.matrices.matrix_strategic(units.values())
+
+"""
