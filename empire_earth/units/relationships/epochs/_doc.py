cf526361e522abf4822b529ead06784ed662f96a
Roy Smart <roytsmart@gmail.com>
2023-04-21 12:19:34 -0600

    Make the unit database dataframe have sticky rows.

--- empire_earth/units/relationships/epochs/_doc.py
+++ empire_earth/units/relationships/epochs/_doc.py
@@ -20,7 +20,7 @@ Units
     units = empire_earth.units.attackers(epoch).values()
     units = [unit for unit in units if unit.theater == theater]
 
-    pandas.DataFrame(units).style.set_sticky('rows')
+    pandas.DataFrame(units).set_index('name').style.set_sticky('rows')
 
 ===========================
 Damage Dealt per Hit
